c0c77bf9ba0e84da664d0a1c422b07fe303dce72
higuchiyumiko <g001057y@mails.cc.ehime-u.ac.jp>
2023-06-10 00:53:41 +0900

    sample_commit

---
+++ sample.php
@@ -0,0 +1,12 @@
+<?php 
+     $p="abc";
+    
+     echo $p.PHP_EOL;
+     
+     $array1=array('a','b','c');
+     
+     foreach($array1 as $a){
+       echo $a;  
+     }
+     
+?>
---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+    $num=$argv[1];
+    
+    
+    switch($num){
+        
+       case 1:
+            echo $num;
+        break;
+
+        case 2:
+            echo $num;
+        break;
+
+     default;
+
+    }  
+?>
